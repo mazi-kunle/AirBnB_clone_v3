a1bce17f4b6e1ad4159ed2fc84237f860d961f1d
mazi-kunle <olakunlemichael644@gmail.com>
2023-02-17 17:58:12 -0800

    3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,14 @@
 
 
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -12,3 +20,18 @@ def status():
                     "status": "OK"
                 }
     return json_data
+
+
+@app_views.route('/stats')
+def stats():
+    '''retrieves the number of each object by type'''
+    data = {
+                "amenities": storage.count(Amenity),
+                "cities": storage.count(City),
+                "places": storage.count(Place),
+                "reviews": storage.count(Review),
+                "states": storage.count(State),
+                "users": storage.count(User)
+            }
+
+    return(jsonify(data))
